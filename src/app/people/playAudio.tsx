74d68cc3986c7d8ce662eac4ef9e02d1a5ab10d3
ParastooMarzi <parastoo.marzi77@gmail.com>
2024-01-01 21:27:35 +0330

    new7

--- src/app/people/playAudio.tsx
+++ src/app/people/playAudio.tsx
@@ -1,26 +1,33 @@
 "use client"
 import { Pause, Play } from 'lucide-react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const PlayAudio = ({ audioUrl }: { audioUrl: string }) => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const audio = new Audio("https://up.myniloomusic2.ir/Niloo/Metallica/Metallica%20-%20Nothing%20Else%20Matters.mp3");
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    setAudio(new Audio("https://up.myniloomusic2.ir/Niloo/Metallica/Metallica%20-%20Nothing%20Else%20Matters.mp3"));
+  }, [audioUrl]);
 
   const toggleAudio = () => {
-    if (isPlaying) {
-      audio.pause();
-      setIsPlaying(false);
-    } else {
-      audio.play();
-      setIsPlaying(true);
+    if (audio) {
+      if (isPlaying) {
+        audio.pause();
+        setIsPlaying(false);
+      } else {
+        audio.play();
+        setIsPlaying(true);
+      }
     }
   };
 
   return (
     <button onClick={toggleAudio}>
-      {isPlaying ?  <Pause />:<Play/>}
+      {isPlaying ? <Pause /> : <Play />}
     </button>
   );
 };
 
 export default PlayAudio;
+
